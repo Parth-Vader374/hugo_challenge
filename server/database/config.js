e56e45d413934ed26812e193aa7a1b5f5cc099ae
Parth Mehta <72329616+Parth-Vader374@users.noreply.github.com>
2023-04-03 13:33:09 -0500

    Update config.js

--- server/database/config.js
+++ server/database/config.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "insuranceApplication",
-  password: "Miami!@#4",
+  user: "your_user",
+  host: "your_host",
+  database: "your_DB",
+  password: "your_pwd",
   port: 5432,
 });
 
